cf92614455803b093542c192e0e715d1c67692c8
dispiche <dispiche@student.ethz.ch>
2022-09-24 14:54:22 +0200

    clean up. Removing unssesary code

--- exploring_pytorch/notepad.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-
-loss = nn.MSELoss()
-input = torch.randn(3, 5, requires_grad=True)
-target = torch.randn(3, 5)
-output = loss(input, target)
-output.backward()
